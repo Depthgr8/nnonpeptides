55da5da44869adb01a9c8a1878f1a180eb6ebc83
depthgr8 <depthgr8@localhost.localdomain>
2017-09-16 21:21:14 +0530

    Cross validation on nnet trained model

--- nnet_on_boruta_features.R
+++ nnet_on_boruta_features.R
@@ -26,7 +26,24 @@ cat("Test cases : ",nrow(test),"\n")
 
 # Training/testing neural networks ----------------------------------------
 
+# Train neural network 
+sink("/dev/null")
 train.mlln <- multinom(train$output~.,train,model = TRUE)
-test.mlln <- predict(train.mlln,test)
-print(confusionMatrix(table(test$output,test.mlln)))
+sink()
 
+# Test neural network using k-fold cross validation
+k <- 10
+nnlog <- matrix(0,k)
+for (i in 1:k)
+{
+  cat(i,"/",k,"iteration\n")
+  indexes = sample(1:nrow(features), size=0.2*nrow(features))
+  test = features[indexes,]
+  train = features[-indexes,]
+  sink("/dev/null")
+  train.mlln <- multinom(train$output~.,train,model = TRUE)
+  sink() 
+  test.mlln <- predict(train.mlln,test)
+  nnlog[i] <- unname(confusionMatrix(table(test$output,test.mlln))$overall[1])
+}
+cat("Accuracy after ",k,"cross validation is :",sum(nnlog)/k*100,"%")
